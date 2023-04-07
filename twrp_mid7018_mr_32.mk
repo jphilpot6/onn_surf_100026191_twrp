835d644d49f28fb04918c27dee74ccb2cea37e8a
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2023-04-07 20:08:01 +0100

    Corrected device path.

--- twrp_mid7018_mr_32.mk
+++ twrp_mid7018_mr_32.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from mid7018_mr_32 device
-$(call inherit-product, device/mid7018_mr_32/device.mk)
+$(call inherit-product, device/onn/mid7018_mr_32/device.mk)
 
 PRODUCT_DEVICE := mid7018_mr_32
 PRODUCT_NAME := twrp_mid7018_mr_32
@@ -20,9 +20,9 @@ PRODUCT_BRAND := onn
 PRODUCT_MODEL := 100026191
 PRODUCT_MANUFACTURER := onn
 
-#PRODUCT_GMS_CLIENTID_BASE := android-digiland
+PRODUCT_GMS_CLIENTID_BASE := android-digiland
 
-#PRODUCT_BUILD_PROP_OVERRIDES += \
+PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="vnd_mid7018_mr_32-user 11 RP1A.200720.011 mp5V4198 release-keys"
 
-#BUILD_FINGERPRINT := onn/100026191/mid7018_mr_32:11/RP1A.200720.011/hkh20220510:user/release-keys
+BUILD_FINGERPRINT := onn/100026191/mid7018_mr_32:11/RP1A.200720.011/hkh20220510:user/release-keys
