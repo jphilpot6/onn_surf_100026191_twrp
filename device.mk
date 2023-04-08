a94e6a66016bb32e9f8e6fc4393975a047ab60f8
jphilpot6 <57756344+jphilpot6@users.noreply.github.com>
2023-04-08 16:36:38 -0400

    Add files via upload

---
+++ device.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/onn/mid7018_mr_32
+
+# Screen Resolution
+TARGET_SCREEN_WIDTH := 1024
+TARGET_SCREEN_HEIGHT := 600
+
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+
+# Treble
+BOARD_VNDK_VERSION := current
+
+# fastboot/d hal
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
